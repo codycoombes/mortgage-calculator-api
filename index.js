df6febd0f68badec8c9075c7c96b73f4012d3672
Cody Coombes <coombes.cody@gmail.com>
2019-04-06 20:09:03 -0700

    Added GET request for mortgage-amount and completed maxMortgage function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js for mortgage-calculator-api 
-// By Cody Coombes
+// Created on April 4
+// Author: Cody Coombes
 
 var express = require('express');
 var tools = require('./tools');
@@ -14,18 +15,18 @@ app.use(body_parser.urlencoded({extended: true}))
 // Get the recurring payment amount of a mortgage
 // Accepts and returns JSON
 // Must have all the fields or returns error
+// Example JSON input: {"asking_price": 700000, "down_payment": 200000, "payment_schedule": "weekly", "amortization_period": 10}
 //
-// INPUT:
-// askingPrice: Asking price in dollars
-// downPayment: Must be at least 5% of first $500k plus 10% of any amount above $500k (So $50k on a $750k mortgage)
-// amortPeriod: Min 5 years, max 25 years
-// paymentSchedule: Weekly, biweekly, monthly
-//
+// PARAMS:
+// asking_price: Asking price in dollars
+// down_payment: Must be at least 5% of first $500k plus 10% of any amount above $500k (So $50k on a $750k mortgage)
+// payment_schedule: Weekly, biweekly, monthly
+// amortization_period: Min 5 years, max 25 years
 // RETURN: Recurring payment amount of a mortgage in JSON format
 app.get('/payment-amount', (req, res) => {
 
     // Validate the input and error check
-    var errors = tools.validate(req.body);
+    var errors = tools.validatePayment(req.body);
 
     if (errors.length) {
         return res.send(tools.errorHandler(errors, 400))
@@ -38,7 +39,7 @@ app.get('/payment-amount', (req, res) => {
         var payment_schedule = req.body.payment_schedule.toLowerCase();
         var amortization_period = req.body.amortization_period;
     	
-        var payment = tools.mortgage(asking_price, down_payment, payment_schedule, amortization_period, interest_rate);
+        var payment = tools.paymentCalculator(asking_price, down_payment, payment_schedule, amortization_period, interest_rate);
         
         if (payment !== -1) {
             res.json(
@@ -59,32 +60,74 @@ app.get('/payment-amount', (req, res) => {
 });
 
 // Get the maximum mortgage amount
+// Accepts and returns JSON
+// Must have all the fields or returns error (except for down_payment which is optional)
+// Example JSON input: {"payment_amount": 700000, "down_payment": 200000, "payment_schedule": "weekly", "amortization_period": 10}
+//
+// PARAMS:
+// payment_amount: The reoccurring payment amount
+// down_payment: Must be at least 5% of first $500k plus 10% of any amount above $500k (So $50k on a $750k mortgage)
+// payment_schedule: Weekly, biweekly, monthly
+// amortization_period: Min 5 years, max 25 years
+// RETURN: Maximum mortgage that can be taken out
 app.get('/mortgage-amount', (req, res) => {
-	return res.send('GET request for mortgage-amount');
+	 // Validate the input and error check
+    var errors = tools.validateMortgage(req.body);
+
+    if (errors.length) {
+        return res.send(tools.errorHandler(errors, 400))
+    }
+
+    else {
+
+        var payment_amount = req.body.payment_amount;
+        var down_payment = req.body.down_payment;
+        var payment_schedule = req.body.payment_schedule.toLowerCase();
+        var amortization_period = req.body.amortization_period;
+        
+        var maximum_mortgage = tools.maxMortgage(payment_amount, down_payment, payment_schedule, amortization_period, interest_rate);
+        
+        if (maximum_mortgage !== -1) {
+            res.json(
+            {
+                maxmium_mortgage : maximum_mortgage,
+                status : 200,
+                timestamp : parseInt(Date.now())
+            });
+        }
+
+        else {
+            errors = "Down payment must be at least 5% of first $500k plus 10% of any amount above $500k (So $50k on a $750k mortgage)";
+            return res.send(tools.errorHandler(errors, 400))
+        }
+    }
 });
 
 // Change the interest rate (%) used by the application
-// INPUT:
-// interestRate: the new interest rate (from 0 to 100)
+// Accepts and returns JSON
+// Example JSON input: {"interest_rate": 5}
+// PARAMS:
+// interest_rate: the new interest rate (from 0 to 100)
 // RETURN: the old and new interest rate
 app.patch('/interest-rate', (req, res) => {
 
-	var old_interest_rate = interest_rate;
+	var old_interest_rate = interest_rate * 100;
     interest_rate = req.body.interest_rate / 100;
+    var new_interest_rate = interest_rate * 100;
 
     // Return JSON object if valid otherwise return error message
     if (typeof interest_rate === "number" && interest_rate >= 0 && interest_rate <= 100) {
     	res.json(
         {
             old_interest_rate: old_interest_rate,
-            new_interest_rate: interest_rate,
+            new_interest_rate: new_interest_rate,
             status: 200,
             timestamp: parseInt(Date.now())
         });
     }
 
     else {
-        var error = "Interest Rate is invalid.";
+        var error = "Interest rate must be ";
         return res.send(tools.errorHandler(error, 400));
     }
 });
--- tools.js
+++ tools.js
@@ -1,5 +1,6 @@
 // tools.js for mortgage-calculator-api 
-// By Cody Coombes
+// Created on April 4
+// Author: Cody Coombes
 
 const PAYMENT_SCHEDULE_TYPES = ["weekly", "biweekly", "monthly"];
 const PAYMENT_SCHEDULE_DICT = {"weekly": 52, "biweekly": 26, "monthly": 12};
@@ -10,7 +11,7 @@ const INSRAUNCE_RATE_3 = 0.018;
 
 module.exports = {
 
-	mortgage: function(asking_price, down_payment, payment_schedule, amortization_period, interest_rate) {
+	paymentCalculator: function(asking_price, down_payment, payment_schedule, amortization_period, interest_rate) {
 
 		var loan_principal = asking_price - down_payment;
 		var down_payment_rate = down_payment / asking_price;
@@ -36,7 +37,6 @@ module.exports = {
 			return -1;
 		}
 
-		
 		// Mortgage insurance is required on all mortgages with less than 20% down. Insurance must be
 		// calculated and added to the mortgage principal. Mortgage insurance is not available for
 		// mortgages > $1 million.
@@ -64,7 +64,6 @@ module.exports = {
 			// else 20%+ N/A
 		}
 
-
 		// Payment formula: P = L[c(1 + c)^n]/[(1 + c)^n - 1]
 		// P = Payment
 		// L = Loan Principal
@@ -76,10 +75,69 @@ module.exports = {
 		return payment;
 	},
 
+	maxMortgage: function(payment_amount, down_payment, payment_schedule, amortization_period, interest_rate) {
+
+		var c = interest_rate / PAYMENT_SCHEDULE_DICT[payment_schedule];
+		var n = amortization_period * PAYMENT_SCHEDULE_DICT[payment_schedule];
+
+		// Payment formula: P = L[c(1 + c)^n]/[(1 + c)^n - 1]
+		// L = P[(1+c)^n-1] / [c(1+c)^n]
+		// P = Payment
+		// L = Loan Principal
+		// c = Interest Rate
+		// n = Frequency of payments
+
+		var loan_principal = ((payment_amount * ((1+c)**n-1)) / (c * (1+c)**n));
+
+		// down_payment is optional
+		if (down_payment > 0) {
+
+			var down_payment_rate = down_payment / loan_principal;
+
+			if (down_payment_rate < INSURANCE_REQUIRED_DOWN) {
+
+				if (down_payment_rate >= 0.05 && down_payment_rate < 0.1) {
+					loan_principal = loan_principal * (1 - INSURANCE_RATE_1);
+				}
+
+				else if (down_payment_rate >= 0.1 && down_payment_rate < 0.15) {
+					loan_principal = loan_principal * (1 - INSURANCE_RATE_2);
+				}
+
+				else if (down_payment_rate >= 0.15 && down_payment_rate < 0.2) {
+					loan_principal = loan_principal * (1 - INSRAUNCE_RATE_3);
+				}
+
+				// else 20%+ N/A
+			}
+		}
+
+		// If no down payment deduct using highest rate
+		else {
+
+			down_payment = loan_principal * (1 - INSURANCE_RATE_1);
+
+			if (loan_principal > 500000) {
+				down_payment = (loan_principal - 500000)*(0.1) + (500000 * 0.05);
+			}
+
+			else {
+				down_payment = (loan_principal * 0.05);
+			}
+		}
+
+		console.log(down_payment);
+		console.log(loan_principal);
+
+		loan_principal = round((loan_principal + down_payment), 2);
+
+		return loan_principal;
+	},
+
 	// Validate that are no missing parameters and parameters are valid
-	// INPUT: JSON object
-	// RETURN: error message
-    validate: function(req) {
+	// PARAMS: JSON object
+	// RETURN: error message or empty message if no errors
+    validatePayment: function(req) {
 
 	    var errors  = [];
 
@@ -92,19 +150,61 @@ module.exports = {
 	    else {
 
 	        if ((typeof req.asking_price !== 'number') || req.asking_price < 0) {
-	            errors.push("Invalid asking price. Must be a number greater than 0.");
+	            errors.push("Asking price must be a number greater than 0.");
 	        }
 
 	        if ((typeof req.down_payment !== 'number') || req.down_payment <= 0 || req.asking_price < req.down_payment) {
-	            errors.push("Invalid down payment. Must be greater than or equal to 0 but smaller than asking price.");
+	            errors.push("Down payment must be greater than or equal to 0 but smaller than asking price.");
 	        }
 
 	        if ((typeof req.payment_schedule !== 'string') || PAYMENT_SCHEDULE_TYPES.indexOf(req.payment_schedule.toLowerCase()) <= -1) {
-	            errors.push("Invalid payment schedule. Must be in format of weekly, biweekly, or monthly.");
+	            errors.push("Payment schedule must be in format of weekly, biweekly, or monthly.");
 	        }
 
 	        if ((typeof req.amortization_period !== 'number') || req.amortization_period < 5 || req.amortization_period > 25) {
-	            errors.push("Invalid amortization period. Must be no less than 5 and no greater than 25.");
+	            errors.push("Amortization must be no less than 5 and no greater than 25.");
+	        }
+	    }
+
+	    if (errors.length) {
+	        console.log(errors);
+	    }
+
+	    return errors;
+	},
+
+	// Validate that are no missing parameters and parameters are valid
+	// INPUT: JSON object
+	// RETURN: error message
+    validateMortgage: function(req) {
+
+	    var errors  = [];
+
+	    // Check if any fields are missing
+	    if (!req.payment_amount || !req.payment_schedule || !req.amortization_period ) {
+	        errors.push("Missing inputs. Need payment amount, payment schedule, and amortization period (Down payment is optional).");
+	    }
+
+	    // Check if fields are valid
+	    else {
+
+	        if ((typeof req.payment_amount !== 'number') || req.payment_amount < 0) {
+	            errors.push("Payment amount must be a number greater than 0.");
+	        }
+
+	        // down_payment is optional so check first if a field was entered
+	        if (req.down_payment) {
+		        if ((typeof req.down_payment !== 'number') || req.down_payment <= 0) {
+		            errors.push("Invalid down payment.");
+		        }
+		    }
+
+	        if ((typeof req.payment_schedule !== 'string') || PAYMENT_SCHEDULE_TYPES.indexOf(req.payment_schedule.toLowerCase()) <= -1) {
+	            errors.push("Payment schedule must be in format of weekly, biweekly, or monthly.");
+	        }
+
+	        if ((typeof req.amortization_period !== 'number') || req.amortization_period < 5 || req.amortization_period > 25) {
+	            errors.push("Amortization must be no less than 5 and no greater than 25.");
 	        }
 	    }
 
